e6cce9103f0a17ac33aeeb38e7b8dda0af5f4c13
David Henderson <dhend1863@gmail.com>
2019-12-02 16:41:35 -0600

    File Structure created

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+const PORT = process.env.PORT || 8080
+
+// create application/json parser
+const jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
