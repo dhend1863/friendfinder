e5589bf33202e934b6def33c8f73a418f83df0bd
David Henderson <dhend1863@gmail.com>
2019-12-04 15:13:01 -0600

    Questions Complete

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+const friends = [
+
+    {
+        name: "Charlie",
+        photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+        scores: [
+            "4",
+            "3",
+            "4",
+
+
+
+        
+        
+        
+        
+        ]
+        
+
+
+    },
+
+
+
+
+]
+
+module.exports = friends;
